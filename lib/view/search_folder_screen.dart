3458a8b07681575a91f004dd36e2e1cba5bea48e
as-android-developer <asatarpk@gmail.com>
2025-08-02 23:00:17 +0500

    get folder restrict based on access

--- lib/view/search_folder_screen.dart
+++ lib/view/search_folder_screen.dart
@@ -98,9 +98,26 @@ class _SearchFolderScreenState extends State<SearchFolderScreen> {
                 title: const Text('Get Folder'),
                 onTap: () async {
                   Navigator.pop(context); // Dismiss bottom sheet
-                  // Use the outer context, not this builder context
-                  await planProvider.copySharedFolderWithTechniques(collection);
-                  await Util.showMessageDialog(context, 'All folder techniques has been copied!');
+                  if (!isInternetAvailable) {
+                    Util.showMessageDialog(context, 'Internet required to access this folder!');
+                    return;
+                  }
+                  if (collection.access == 'private') {
+                    Util.showMessageDialog(context, 'You do not have access to this folder!');
+                    return;
+                  } else if (collection.access == 'specific' &&
+                      !collection.allowedUsers.contains(planProvider.loggedUserId)) {
+                    Util.showMessageDialog(context, 'You do not have access to this folder!');
+                    return;
+                  }
+
+                  if (await planProvider.checkFolderPermission(planProvider.loggedUserId, collection.id!)) {
+                    await planProvider.copySharedFolderWithTechniques(collection);
+                    await Util.showMessageDialog(context, 'All folder techniques has been copied!');
+                  } else {
+                    Util.showMessageDialog(context, "You do not have access to this folder!");
+                  }
+
                 },
               ),
             ],
